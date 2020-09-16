7e89484226d3567ffd2f39245a189491537b27b1
devasis <devasis.majhi@gmail.com>
2020-09-16 20:43:33 +0530

    fabicon

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ dotenv.config();
 const distPath = path.join(__dirname, 'dist');
 
 const staticResources = path.join(__dirname, 'static');
+const fabIcons = path.join(__dirname, 'fab');
 app.use('/dist', express.static(distPath));
 app.use('/static', express.static(staticResources));
+app.use('/fab', express.static(fabIcons));
 
 app.get('/', (req, res) => {
     console.log('/ invoked');
