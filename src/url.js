17a636eaede7b098d763a95c96a13a1831cc50ac
devasis <devasis.majhi@gmail.com>
2020-09-16 20:34:38 +0530

    wss

--- src/url.js
+++ src/url.js
@@ -1,4 +1,4 @@
 const CARD_ADDA_SERVRE_URL = 'https://card-adda-server.herokuapp.com';
-const CARD_ADDA_WS_URL = 'ws://card-adda-server.herokuapp.com'
+const CARD_ADDA_WS_URL = 'wss://card-adda-server.herokuapp.com'
 
 export {CARD_ADDA_SERVRE_URL, CARD_ADDA_WS_URL};
